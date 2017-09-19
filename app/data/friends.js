d20c49759f5af1583f3837901cd0a15b55e19e6d
Lebeza Wendemsesha <BezMack@Lebezas-MBP.fios-router.home>
2017-09-18 20:56:55 -0400

    modified files

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+{
+  "name":"Hella",
+  "photo":"https://i0.wp.com/youmeandtrends.com/wp-content/uploads/2015/09/cute-love-wallpaper.jpg?resize=640%2C480",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Cutie",
+  "photo":"http://pixdaus.com/files/items/pics/7/56/205756_1249371d8782bbe0951cb619411ab34d_large.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Abby",
+  "photo":"http://1.bp.blogspot.com/--omILa7qBhg/UQK78f47AWI/AAAAAAAAAjA/43_s3e9wAAs/s1600/sad+alone+love+girls+hd+wallpaper+(7).jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+
+]
+
+module.exports = friends;
---
+++ app/public/script.js
@@ -0,0 +1,33 @@
+
+// Form Validation
+// ===================================
+
+    /*function validateForm(){
+
+      var surveyArray = [];
+      var surveyScores = $(".sRange");
+      for (var i = 0; i < surveyScores.length; i++) {
+        surveyArray.push(parseInt(surveyScores[i].options[surveyScores[i].selectedIndex].value))
+      }
+
+      var name = $("#sName").val().trim();
+      var photo = $("#sPhoto").val().trim();
+
+      if (name === "" || photo === ""){
+        alert("Fields can not be empty")
+        return false;
+      } else {
+
+        var newFriend =  {
+              name: name,
+              photo: photo,
+              scores: surveyArray,
+        }
+        return newFriend;
+      }
+    }
+
+
+  })
+
+})*/
